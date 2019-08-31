ebbcd33a5f649088612a90dd95a90d4a76c9e205
Ali A. Dhillon <ali.dhillon102@gmail.com>
2019-09-01 00:21:53 +0500

    removes unused use classes from TestController

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,11 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use ErrorException;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Crypt;
-use Illuminate\Contracts\Encryption\DecryptException;
-use Illuminate\Support\Facades\Hash;
+
 
 class TestController extends Controller
 {
